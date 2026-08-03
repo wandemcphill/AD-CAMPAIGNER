40326295adb8772a40f91304ca0fd6b5908a4b35
Omotunde Oni <vidzimedialtd@gmail.com>
2026-08-03 17:16:43 +0100

    Provide queue producer to digital value module

--- apps/api/src/modules/digital-value/digital-value.module.ts
+++ apps/api/src/modules/digital-value/digital-value.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { DigitalValueService } from './digital-value.service';
 import { DigitalValueController, AdminDigitalValueController } from './digital-value.controller';
 import { PrismaModule } from '../prisma.module';
+import { QueueProducerService } from '../queue-producer.service';
 import { FxModule } from '../fx/fx.module';
 
 @Module({
   imports: [FxModule, PrismaModule],
-  providers: [DigitalValueService],
+  providers: [QueueProducerService, DigitalValueService],
   controllers: [DigitalValueController, AdminDigitalValueController],
   exports: [DigitalValueService]
 })
